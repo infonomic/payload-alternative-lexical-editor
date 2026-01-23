3f1f2e0774ac0ba87be02882d228fc46e644280c
Anthony Bouch <anthony@infonomic.io>
2026-01-23 11:21:17 +0700

    feat: updated helper to createEmptyState to match default editor state

--- packages/payload-alternative-lexical-editor/src/validate/createEmptyEditorState.ts
+++ packages/payload-alternative-lexical-editor/src/validate/createEmptyEditorState.ts
@@ -3,75 +3,49 @@ import type {
   SerializedLexicalNode,
   SerializedParagraphNode,
   SerializedRootNode,
-  SerializedTextNode,
 } from 'lexical'
 
 /**
  * createEmptyEditorState
  * @returns 
- * root: {
-      // Lexical does not allow the root to have zero children.
-      // Represent an "empty" document as a single empty paragraph.
-      children: [
-        {
-          type: 'paragraph',
-          format: '',
-          indent: 0,
-          version: 1,
-          direction: 'ltr',
-          children: [
-            {
-              type: 'text',
-              text: '',
-              format: 0,
-              style: '',
-              mode: 0,
-              detail: 0,
-              direction: 'ltr',
-              indent: 0,
-              version: 1,
-            },
-          ],
-        },
-      ],
-      direction: 'ltr',
-      format: '',
-      indent: 0,
-      type: 'root',
-      version: 1,
-    },
+ * "root": {
+        "children": [{
+                "children": [],
+                "direction": null,
+                "format": "",
+                "indent": 0,
+                "type": "paragraph",
+                "version": 1,
+                "textFormat": 0,
+                "textStyle": ""
+            }
+        ],
+        "direction": null,
+        "format": "",
+        "indent": 0,
+        "type": "root",
+        "version": 1
+    }
  */
 export function createEmptyEditorState(): SerializedEditorState<SerializedLexicalNode> {
-  // Lexical does not allow the root to have zero children.
-  // Represent an "empty" document as a single empty paragraph.
-  const emptyTextNode: SerializedTextNode = {
-    type: 'text',
-    text: '',
-    format: 0,
-    style: '',
-    mode: 'normal',
-    detail: 0,
-    version: 1,
-  }
-
   const emptyParagraphNode: SerializedParagraphNode = {
-    type: 'paragraph',
+    children: [],
+    direction: null,
     format: '',
     indent: 0,
+    type: 'paragraph',
+    version: 1,
     textFormat: 0,
     textStyle: '',
-    version: 1,
-    direction: 'ltr',
-    children: [emptyTextNode],
   }
 
   const rootNode: SerializedRootNode = {
-    type: 'root',
-    version: 1,
-    direction: 'ltr',
+    children: [emptyParagraphNode],
+    direction: null,
     format: '',
     indent: 0,
-    children: [emptyParagraphNode],
+    type: 'root',
+    version: 1,
   }
 
   return { root: rootNode }
